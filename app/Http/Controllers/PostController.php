b3769b3376ebd1c01ab85f48618cc33bd2aebb9b
Reuvin Hernandez <reuvinhernandez22@gmail.com>
2024-02-27 16:03:45 +0800

    Update

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\PostRequest;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $post = Post::all();
+        if(empty($post)){
+            return response()->empty();
+        }
+        return $post;
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(PostRequest $request)
+    {
+        $post = Post::create($request->validated());
+        if(!$post){
+            return response()->failed('Failed to insert data');
+        }else {
+            return response()->success($post, 'Data Inserted Successfully');
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $postId = Post::find($id);
+        if(empty($postId)){
+            return response()->empty();
+        }
+        return $postId;
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(PostRequest $request, string $id)
+    {
+        $postId = Post::find($id);
+        if(empty($postId)){
+            return response()->empty();
+        }
+        if(!$postId){
+            return response()->failed('Failed to Update Data');
+        }else {
+            return response()->success($postId, 'Data Updated Successfully');
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $postId = Post::find($id);
+        if(empty($postId)){
+            return response()->empty();
+        }
+        if(!$postId){
+            return response()->failed('Failed to Delete Post');
+        }else {
+            return response()->json([
+                'status' => 200,
+                'message' => 'Data Deleted Successfully'
+            ], 200);
+        }
+    }
+}
